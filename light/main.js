0fb06458e4beb8183961b5eb7d80d09494ceba92
ymkmy98 <ymkmy98@gmail.com>
2023-06-25 00:23:49 +0900

    Tutorial for camera, camera-controls

--- light/main.js
+++ light/main.js
@@ -1,5 +1,6 @@
 import * as THREE from "./build/three.module.js";
 import { OrbitControls } from "./controls/OrbitControls.js";
+import { RectAreaLightHelper } from "./helpers/RectAreaLightHelper.js";
 
 //UIデバッグ
 const gui = new dat.GUI();
@@ -26,8 +27,66 @@ camera.position.z = 4;
 scene.add(camera);
 
 //ライト
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
+// アンビエントライト
+const ambientLight = new THREE.AmbientLight();
+ambientLight.color = new THREE.Color(0xffffff);
+ambientLight.intensity = 0.5;
+ambientLight.position.x = 2;
+// scene.add(ambientLight);
+
+gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
+
+// ディレクショナルライト
+const directionalLight = new THREE.DirectionalLight(0x0fffff, 0.5);
+directionalLight.position.set = (0, 0.55, 5);
+scene.add(directionalLight);
+
+// ヘミスフィアライト
+// HemisphereLight(空の色, 地面の色, 光の強さ)
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3);
+hemisphereLight.position.set(0, 0.55, 5);
+scene.add(hemisphereLight);
+
+// ポイントライト
+// PointLight(色, 光の強さ, 距離, 減衰率)
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2);
+pointLight.position.set(-1, -0.5, 1);
+scene.add(pointLight);
+
+// rectAreaLight
+// RectAreaLight(色, 光の強さ, 幅, 高さ)
+// MeshStandardMaterialもしくはMeshPhysicalMaterialを使用する必要がある
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 3, 1);
+rectAreaLight.position.set(-1.5, 0, 1.5);
+scene.add(rectAreaLight);
+
+// spotLight
+// SpotLight(色, 光の強さ, 距離, 角度, 輪郭のぼやけ, パワー)
+const spotLight = new THREE.SpotLight(0x78ff00, 3, 10, Math.PI * 0.1, 0.25, 1);
+spotLight.position.set(0, 1, 1);
+console.log(spotLight.target);
+spotLight.target.position.x = -1;
+scene.add(spotLight, spotLight.target);
+
+// ヘルパー
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2);
+scene.add(directionalLightHelper);
+
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2);
+scene.add(hemisphereLightHelper);
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2);
+scene.add(pointLightHelper);
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(spotLightHelper);
+window.requestAnimationFrame(() => {
+  spotLightHelper.update();
+});
+
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight);
+scene.add(rectAreaLightHelper);
+
 
 //マテリアル
 const material = new THREE.MeshStandardMaterial();
--- material/controls/FlyControls.js
+++ material/controls/FlyControls.js
@@ -2,7 +2,7 @@ import {
 	EventDispatcher,
 	Quaternion,
 	Vector3
-} from '../../../build/three.module.js';
+} from '../build/three.module.js';
 
 const _changeEvent = { type: 'change' };
 
